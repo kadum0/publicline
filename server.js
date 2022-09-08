e4a9149f1953bca3b1ae7122aa5f4205d672863e
kadum <75564464+kadum2@users.noreply.github.com>
2022-09-08 21:17:03 +0300

    remove dub, calc routes

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 
+// import env from 'dotenv'
+// const {pkg} = pkg
+
 
 // firebase 
+import {createRequire} from 'module'
+const require = createRequire(import.meta.url)
+
+require("dotenv").config()
 
 import { initializeApp } from "firebase/app";
+
 
 import {getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signOut, signInWithEmailAndPassword} from "firebase/auth"
 
@@ -16,26 +24,6 @@ import { getFirestore, onSnapshot,
 import {getStorage, ref, uploadBytes, getDownloadURL, listAll, list} from 'firebase/storage'
 
 
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// import { getFirestore, collection, onSnapshot, doc, getDocs, getDoc, addDoc, deleteDoc, setDoc, query, where, orderBy, serverTimestamp, updateDoc } from "firebase/firestore"
-
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const publicLineConfig = {
-//   apiKey: "AIzaSyApbg3hYeFDQyE7c8r6Y9jf46sC6OkBBVc",
-//   authDomain: "fir-learning-51196.firebaseapp.com",
-//   projectId: "fir-learning-51196",
-//   storageBucket: "fir-learning-51196.appspot.com",
-//   messagingSenderId: "185617507245",
-//   appId: "1:185617507245:web:5dd806dd7144116d4db724",
-//   measurementId: "G-ZB3PEXP3GM"
-// };
 
 const publicLineConfig = {
     apiKey: "AIzaSyAF-kHhmhnZ2z6GDRhX3YK6ZeN1wQifC8M",
@@ -55,10 +43,8 @@ const publicLineAuth = getAuth(publicLine)
 
 // make server account; claim token; 
 
-signInWithEmailAndPassword(publicLineAuth ,'server@ivc.com', 'thesecretword').then((cred)=>{
+signInWithEmailAndPassword(publicLineAuth ,process.env.EM, process.env.PW).then((cred)=>{
     console.log('registered', cred)
-    // cred.idToken.getIdTokenResult().then(idTokenResult => {console.log(idTokenResult.claims)})
-    // cred.getIdTokenResult().then(idTokenResult => {console.log(idTokenResult.claims)})
 
 onAuthStateChanged(publicLineAuth, async (user)=>{
     if(user){
@@ -81,25 +67,29 @@ onSnapshot(collectionRef, (data)=>{
     data.docs.forEach(doc=>{
         docs.push({...doc.data(), id: doc.id})
     })
-    console.log(docs)
+    // console.log(docs)
 
 
-        ////make the basic routes calc
+        ////
     docs.forEach(e=>{
         // not to allow multiple voting for the route; e.upvotes, e.downvotes
         e.upvotes.forEach(ee=>{
             if(e.downvotes.includes(ee)){
                 // not good user; delete 
-                let userNameIndex = colors.indexOf(e.downvotes.filter(ev=>ev==ee));
-                colors.splice(userNameIndex, 1); 
+                let userNameIndex = e.downvotes.indexOf(e.downvotes.filter(ev=>ev==ee));
+                e.downvotes.splice(userNameIndex, 1); 
             }
         })
 
-        
         //////calculate the route 
-        if(e.downvotes.length+e.upvotes.length == 20)
-        if (e.downvotes.length > e.downvotes.length){
-            // delete the route 
+        // console.log(e)
+        if(e.downvotes.length+e.upvotes.length == 3){
+
+            console.log('time to calc')
+            if (e.downvotes.length > e.upvotes.length){
+                // delete the route 
+                deleteDoc(doc(publicLineDb, 'routes', e.id)).then(()=>console.log('deleted'))
+            }
         }
     })
 
