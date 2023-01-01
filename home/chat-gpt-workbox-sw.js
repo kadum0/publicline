71ede8a10291c08425b66161764caf84253a16ad
kadum <75564464+kadum2@users.noreply.github.com>
2023-01-01 16:52:16 +0300

    chat-gpt-workbox-sw

---
+++ home/chat-gpt-workbox-sw.js
@@ -0,0 +1,27 @@
+
+// Import Workbox
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+// Set up a CacheFirst strategy for files
+workbox.routing.registerRoute(
+  /\.(?:html|css|js|png)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'static-resources',
+  })
+);
+
+// Set up a NetworkFirst strategy for Mapbox map tile images
+workbox.routing.registerRoute(
+  /https:\/\/api\.mapbox\.com/,
+  new workbox.strategies.NetworkFirst({
+    cacheName: 'map-tiles',
+  })
+);
+
+workbox.routing.registerRoute(
+    /leaflet/,
+    new workbox.strategies.NetworkFirst({
+      cacheName: 'map-leaflet',
+    })
+  );
+  
