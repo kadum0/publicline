edd54c5a63778f0977e9178b83f0034fecf07b50
kadum <75564464+kadum2@users.noreply.github.com>
2023-01-03 19:52:45 +0300

    style and sw fix

--- home/chat-gpt-workbox-sw.js
+++ home/chat-gpt-workbox-sw.js
@@ -3,12 +3,20 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 // Set up a CacheFirst strategy for files
+// workbox.routing.registerRoute(
+//   /\.(?:html|css|js|png)$/,
+//   new workbox.strategies.CacheFirst({
+//     cacheName: 'static-resources',
+//   })
+// );
+
 workbox.routing.registerRoute(
   /\.(?:html|css|js|png)$/,
-  new workbox.strategies.CacheFirst({
+  new workbox.strategies.NetworkFirst({
     cacheName: 'static-resources',
   })
 );
+
 
 // Set up a NetworkFirst strategy for Mapbox map tile images
 workbox.routing.registerRoute(
